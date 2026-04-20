cfc85d48977f7d3655994a487615917dbd4b6cf5
unknown <git@github.com:sumitrijal12/pytho.git>
2026-04-20 20:30:08 +0545

    initial commit

---
+++ p.py
@@ -0,0 +1,187 @@
+"""a=5
+b=10
+print(type(a))
+print(a**b"""
+
+"""a = "10"
+b = "11"
+print(int(a)+int(b))
+a = "12"
+print(int(a))"""
+
+
+
+
+
+
+
+
+
+'''
+a = True
+b = True
+print (1==2 and "test"!="hello")
+print(1==2 or "test"!="hello")
+print(not(a))'''
+
+
+"""
+
+
+a = 10
+b = 11
+a = input("Enter any value ")
+b = int(a)
+print(type(a))
+print("user entered",a )
+ 
+ 
+ 
+a = int(input("Enter any value "))
+"""
+
+
+"""
+    age = 20
+country = "Nepal"
+
+
+if age > 18:
+    if country == "Nepal".lower():
+        print(" eligible")
+        else:
+    print("not eligible")
+else:
+    print("minor")"""
+
+
+a=[1,2,3,4,5,6]
+print(type(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
