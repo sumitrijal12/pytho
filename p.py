8cdaa8fe23c51bee915700873a617b39516cc22e
sumitrijal12 <riyachmagar@gmail.com>
2026-04-20 20:52:17 +0545

    Update p.py

--- p.py
+++ p.py
@@ -1,1 +1,431 @@
+"""a=5
+b=10
+print(type(a))
+print(a**b"""
+
+"""a = "10"
+b = "11"
+print(int(a)+int(b))
+a = "12"
+print(int(a))"""
+
+
+
+
+
+
+
+
+
+'''
+a = True
+b = True
+print (1==2 and "test"!="hello")
+print(1==2 or "test"!="hello")
+print(not(a))'''
+
+
+"""
+
+
+a = 10
+b = 11
+a = input("Enter any value ")
+b = int(a)
+print(type(a))
+print("user entered",a )
+ 
+ 
+ 
+a = int(input("Enter any value "))
+"""
+
+
+"""
+    age = 20
+country = "Nepal"
+
+
+if age > 18:
+    if country == "Nepal".lower():
+        print(" eligible")
+        else:
+    print("not eligible")
+else:
+    print("minor")"""
+
+
+a=[1,2,3,4,5,6]
+print(type(a))
+
+
+"""a=5
+b=10
+print(type(a))
+print(a**b"""
+
+"""a = "10"
+b = "11"
+print(int(a)+int(b))
+a = "12"
+print(int(a))"""
+
+
+
+
+
+
+
+
+
+'''
+a = True
+b = True
+print (1==2 and "test"!="hello")
+print(1==2 or "test"!="hello")
+print(not(a))'''
+
+
+"""
+
+
+a = 10
+b = 11
+a = input("Enter any value ")
+b = int(a)
+print(type(a))
+print("user entered",a )
+ 
+ 
+ 
+a = int(input("Enter any value "))
+"""
+
+
+"""
+    age = 20
+country = "Nepal"
+
+
+if age > 18:
+    if country == "Nepal".lower():
+        print(" eligible")
+        else:
+    print("not eligible")
+else:
+    print("minor")"""
+
+
+a=[1,2,3,4,5,6]
+print(type(a))
+
+
+
+a=[]
+name=['nasa','a','da','ba']
+name.append('kaer')
+name.append([1,2,3])
+print(name)
+
+
+
+a=[]
+name=['nasa','a','da','ba']
+name.insert(4,'kaer')
+name.insert(2,[1,2,3])
+print(name)
+
+
+
+a=[1,2]
+b=[2,3]
+a.extend(b)
+print(a)
+print(b)
+b.extend(a)
+print(b)
+a.extend(b)
+print(a)
+
+
+print("......."*3)
+a = [1,2]
+b = [3,4]
+ 
+c = a+b
+print(c)
+print(a, b)
+ 
+
+
+
+
+a = [1,2]
+b = [3,4]
+a.extend(b)
+print(a)
+print(b)
+b.extend(a)
+print(b)
+a.extend(b)
+print(a)
+
+
+
+
+name.insert(2,"insert")
+name.insert(20,"should be error")
+print(name)
+
+
+
+name.append("krish")
+name.append([1,2,3])
+
+
+
+a = []
+name = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris','Ihsan']
+ 
+name = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris','Ihsan']
+print(name[1:5])
+print(name[1:])
+print(name[:6])
+print(name[:])
+
+
+
+name = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris','Ihsan']
+
+print(name)
+
+
+
+data = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris','Ihsan']
+del data[1]
+print(data)
+
+
+data = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris','Ihsan']
+data.remove('Haris')
+print(data)
+
+deleted_name = data.pop()
+print(data)
+print(deleted_name)
+
+
+
+
+data.clear()
+print(data)
+
+
+teachers = ['Nasir', 'Irfan', 'Haris', 'Sheraz', 'Farhan', 'Khalil','Haris', 'Ihsan']
+teachers.sort(reverse=True)
+print(teachers)
+
+
+data = ['Nasir', 'Irfan', 'insert', 'Haris', 'Sheraz', 'Farhan', 'Khalil', 'Haris', 'Ihsan', 'krish', [1, 2, 3], 'should be error']
+print(data[10][1])
+list_data =data[10]
+print(list_data[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
